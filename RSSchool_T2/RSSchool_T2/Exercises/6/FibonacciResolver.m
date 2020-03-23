01c3140eaecb72f2a58298472e4dbb7a8f56ddd6
Alexander Porshnev <porshnev.alexander@gmail.com>
2020-03-23 21:13:25 +0300

    feat: implement 6 exercise

--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,34 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
-    return @[];
+    
+    NSNumber *isEqual = @0;
+    NSNumber *one;
+    NSNumber *two;
+    NSMutableArray *fibonacciSeries = [NSMutableArray new];
+    [fibonacciSeries addObject:@0];
+    int total = 0;
+    int prev = 1;
+    for (int i = 1; i < number.intValue; i++){
+        total = total + prev;
+        prev = total - prev;
+        [fibonacciSeries addObject:[NSNumber numberWithInt:total]];
+    }
+    for (int i = 0; i < (fibonacciSeries.count - 1); i++) {
+        NSNumber *first = [fibonacciSeries objectAtIndex:i];
+        NSNumber *next = [fibonacciSeries objectAtIndex:i+1];
+        if ((first.intValue * next.intValue) == number.intValue) {
+            isEqual = @1;
+            one = first;
+            two = next;
+            break;
+        } else if ((first.intValue * next.intValue) > number.intValue) {
+            one = first;
+            two = next;
+            break;
+        }
+    }
+        
+    return [[NSArray alloc] initWithObjects:one, two, isEqual, nil];
 }
 @end
