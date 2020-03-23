f149780db883b97b5bfe87296279c2f9382c183e
Alexander Porshnev <porshnev.alexander@gmail.com>
2020-03-23 21:04:02 +0300

    feat: implement 3 exercise

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -3,6 +3,34 @@
 @implementation AbbreviationMaker
 // Complete the abbreviation function below.
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
-    return @"Test";
+    
+    NSMutableString *mutA = [a mutableCopy];
+    
+    for (NSUInteger i = 0; i < [mutA length]; i++) {
+        if (i < [b length]) {
+        NSRange range = NSMakeRange(i, 1);
+        NSString *letterA = [mutA substringWithRange:range];
+        NSString *letterAMut = [letterA capitalizedString];
+        NSString *letterB = [b substringWithRange:range];
+            if (![letterA isEqualToString:letterB]) {
+                [mutA replaceOccurrencesOfString:letterA
+                                      withString:letterAMut
+                                         options:0
+                                           range:NSMakeRange(i, 1)];
+                if (![letterAMut isEqualToString:letterB]) {
+                    [mutA deleteCharactersInRange:NSMakeRange(i, 1)];
+                    i--;
+                }
+            }
+        } else {
+        [mutA deleteCharactersInRange:NSMakeRange(i, 1)];
+        }
+    }
+    
+    if ([mutA isEqualToString:b]) {
+        return @"YES";
+    } else {
+        return @"NO";
+    }
 }
 @end
