915df7dc30ed34a16061cfc40a0be95d2c2830d8
Alexander Porshnev <porshnev.alexander@gmail.com>
2020-03-23 20:42:20 +0300

    feat: implement 1 Exercises

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -3,7 +3,23 @@
 @implementation Dates
 
 - (NSString *)textForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
-    return @"";
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    
+    NSString *dateString = [[NSString alloc] initWithFormat:@"%@-%@-%@", day, month, year];
+    [dateFormatter setDateFormat:@"dd-MM-yyyy"];
+    NSDate *date = [dateFormatter dateFromString:dateString];
+    
+    if (!date) {
+        return @"Такого дня не существует";
+    }
+            
+    NSDateFormatter * finalFormatter =  [[NSDateFormatter alloc] init];
+    NSLocale *locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru"];
+    [finalFormatter setLocale:locale];
+    [finalFormatter setDateFormat:@"dd MMMM, EEEE"];
+    NSString *finalString = [finalFormatter stringFromDate: date];
+    [finalString lowercaseString];
+    return finalString;
 }
-
 @end
